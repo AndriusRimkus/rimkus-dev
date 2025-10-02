95bc41e75b648d68c5ad908d7d6d2b4ce437a8e6
Andrius Rimkus <rimkusrimkus@gmail.com>
2025-10-02 17:53:17 +0300

    feat: scaffold a blog using Astro and add the first note

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,21 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://rimkus.dev',
+
+    integrations: [mdx(), sitemap()],
+
+    markdown: {
+        shikiConfig: {
+            theme: 'dracula-soft',
+        },
+    },
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
