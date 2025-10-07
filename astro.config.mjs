d48f719983b8b6d94c1003a9affed51b5ab3fe73
Andrius Rimkus <rimkusrimkus@gmail.com>
2025-10-07 16:48:44 +0300

    Add Vue support. Add Shadcn.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://rimkus.dev',
 
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), vue()],
 
     markdown: {
         shikiConfig: {
